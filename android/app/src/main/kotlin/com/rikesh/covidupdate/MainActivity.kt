3bb2a2aaa1dca1ae99fba516ece7e762ed7c6cd9
rikarma <rikeshkarmacharya.rk@gmail.com>
2020-07-05 15:00:43 +0545

    Initial commit

---
+++ android/app/src/main/kotlin/com/rikesh/covidupdate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rikesh.covidupdate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CovidUpdate());
+}
+
+class CovidUpdate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('COVID Update'),
+    );
+  }
+}
