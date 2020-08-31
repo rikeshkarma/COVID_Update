af55a3b782284bc2d470f95d69ea11dca4208bf0
ShubhDhital <shubhdhital@gmail.com>
2020-08-31 10:53:44 +0545

    news complete cha lagbhag stats ko chai halka bar halnu parcha bar diagram types ko aru j hos cha

--- lib/news.dart
+++ lib/news.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 class News extends StatefulWidget {
   @override
@@ -6,14 +11,82 @@ class News extends StatefulWidget {
 }
 
 class _NewsState extends State<News> {
+  bool nloading = true;
+  var data;
+  var news;
+
+  _launchURL(String website) async{
+    var url = '$website';
+    if (await canLaunch(url)){
+      await launch(url);
+    } else {
+      throw 'could not launch $url';
+    }
+  }
+
+  //for getting news
+
+  Future getLatestNews() async{
+    String url ="https://nepalcorona.info/api/v1/news";
+    var response = await http.get(Uri.encodeFull(url),headers:{'Accept':'application/json'});
+    var jsonData = json.decode(response.body);
+
+    setState(() {
+      news = jsonData['data'];
+      nloading =false;
+      print(data);
+
+    });
+  }
+
+  @override
+  void initState() {
+        super.initState();
+    getLatestNews();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: Scaffold(
-          body: Text('Current News'),
+    return Scaffold(
+      appBar: AppBar(title: Text("Latest Corona News In Nepal"),) ,
+      body: nloading == true ? Center(child: CircularProgressIndicator(),):SingleChildScrollView(
+        child: Column(
+            children: <Widget>[
+
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: news.length,
+                itemBuilder: (context,index){
+                  return Card(
+                    child: Column(
+                      children: <Widget>[
+                        Image.network(news[index]['image_url']),
+                        ListTile(
+                          title: Text(news[index]['title'],style: TextStyle(fontWeight: FontWeight.bold),),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(news[index]['summary']),
+                              OutlineButton(
+                                onPressed: () async {
+                                  _launchURL(news[index]['url']);
+                                },
+                                child: Text('Read More'),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              )
+
+              ]
         ),
       ),
     );
-  }
+     }
 }
